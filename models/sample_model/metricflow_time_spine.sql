b2a2551885c97df58b20dc82722514bd8a1bbdf7
sonesuke <iamsonesuke@gmail.com>
2024-03-24 17:20:50 +0900

    first commit

---
+++ models/sample_model/metricflow_time_spine.sql
@@ -0,0 +1,19 @@
+
+with days as (
+
+    {{
+        dbt.date_spine(
+            'day',
+            "DATE '2000-01-01'",
+            "DATE '2027-01-01'"
+        )
+    }}
+
+),
+
+final as (
+    select cast(date_day as date) as date_day
+    from days
+)
+
+select * from final
